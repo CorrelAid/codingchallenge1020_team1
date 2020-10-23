0ef28456825b52f09abcb0afa7dcad3a66212a0a
Steffen Berhorst <Steffen-Berhorst@web.de>
2020-10-23 13:46:15 +0200

    Added function to randomly inject noise in audio segment

--- preprocessing.py
+++ preprocessing.py
@@ -24,3 +24,14 @@ def change_pitch(audio_sample, low=1.5, high=4):
     pitch_factor = np.random.uniform(low, high) * random.choice([1, -1])
 
     return librosa.effects.pitch_shift(audio_sample, sr=48000, n_steps=pitch_factor)
+
+
+def noise_injection(audio_sample, low=0.001, high=0.01):
+
+    if isinstance(audio_sample, str) and audio_sample.split(".")[-1] == ".wav":
+        audio_sample = librosa.load(audio_sample, sr=48000)
+
+    noise_level = np.random.uniform(low, high)
+    noise = np.random.normal(0, noise_level, len(audio_sample))
+
+    return audio_sample + noise
