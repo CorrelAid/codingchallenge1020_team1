b8e051971f055ae80f4ed6bd36a0c99ac8391d2b
Steffen Berhorst <Steffen-Berhorst@web.de>
2020-10-23 12:44:15 +0200

    Add function to unify whole dataset in regard of samplerate and channels

--- data_loader.py
+++ data_loader.py
@@ -6,6 +6,120 @@ import os
 import csv
 
 
+def build_new_trainingset(path_original="data\\audio_files",
+                          path_additional="data\\AdditionalUtterances\\latest_keywords\\",
+                          path_new_dataset="data\\training", train_csv_path="Train.csv",
+                          energy_threshold=0.18, target_samplerate=48000,
+                          target_channels=1, corrected=True):
+    """A function to convert all utterances to unified sample rate and channel audio files
+
+        :param path_original: Path to directory where .wav files are located
+        :param path_additional: Path to the directory in which the class directories are found
+        :param path_new_dataset: Path to directory where new, converted dataset should be saved
+        :param train_csv_path: Path to the Train.csv file
+        :param energy_threshold: Energy threshold under which an audio sample gets discarded
+        :param target_samplerate: Sample rate that all data gets converted to
+        :param target_channels: Target channel number all data gets converted to. Needs to be 1 or 2.
+        :param corrected: Boolean that shows whether additional data should be gotten from a subdirectory
+        called "corrected", as would be the result of correct_additional_utterances
+        :return: labels, a dictionary containing mappings from all training file names to their labels
+        """
+
+    # First deal with original data
+    # Load Train.csv file
+    csv_file = open(train_csv_path, newline='')
+    train_csv = csv.reader(csv_file, delimiter=',')
+    # Call iterator one time to get rid of first row that only contains descriptions
+    train_csv.__next__()
+
+    if not os.path.isdir(path_new_dataset):
+        os.mkdir(path_new_dataset)
+        print("Creating new folder for data at {}".format(os.path.abspath(path_new_dataset)))
+
+    labels = {}
+    ignored_audio = []
+
+    print("Starting iteration through original data.")
+
+    for row in train_csv:
+        filename, label = row
+        # Get only name of wav file without rest of path in Train.csv
+        filename = filename.split('/')[-1]
+        # Convert string label to number label
+
+        # Using os.path.join for all path operations to make it OS agnostic
+        file_path = os.path.join(path_original, filename)
+
+        # Load signal and calculate total energy
+        signal, sr = librosa.load(file_path, sr=44100)
+        energy = np.sum(librosa.feature.rms(signal))
+
+        # Skip if energy is below threshold
+        if energy <= energy_threshold:
+            ignored_audio.append(filename)
+            continue
+
+        file_new_path = os.path.join(path_new_dataset, filename)
+        command_to_run = "ffmpeg -i {} -ac {} -ar {} {}".format(file_path, target_channels, target_samplerate, file_new_path)
+        os.system(command_to_run)
+
+        labels[filename] = label
+
+    print("Finished original data, iterating through additional data now.")
+    # Now go through additional utterances and do the same
+    # Create list of all class folders
+    class_dirs = os.listdir(path_additional)
+
+    # Catch some pathing mistakes (though not all)
+    if len(class_dirs) == 0:
+        print("The given directory is empty.")
+        return
+
+    elif len(class_dirs[0].split(".")) != 1:
+        print("The first entry in given directory is not a folder. You should"
+              "give the path to the directory in which the class-subfolders are.")
+        return
+
+    for class_dir in class_dirs:
+        # Path to distinctive class subfolder
+        class_path = os.path.join(path_additional, class_dir)
+        if corrected:
+            class_path = os.path.join(path_additional, class_dir, "corrected")
+
+        # List of all audio files in respective class directory
+        audio_files = os.listdir(class_path)
+
+        for filename in audio_files:
+            file_path = os.path.join(class_path, filename)
+
+            # Load signal and calculate total energy
+            signal, sr = librosa.load(file_path, sr=44100)
+            energy = np.sum(librosa.feature.rms(signal))
+
+            # Skip if energy is below threshold
+            if energy <= energy_threshold:
+                ignored_audio.append(filename)
+                continue
+
+            file_new_path = os.path.join(path_new_dataset, filename)
+
+            # Run FFMPEG command on CMD to convert audio file to target location (in new subfolder)
+            # with new samplerate and channel number
+            command_to_run = "ffmpeg -i {} -ac {} -ar {} {}".format(file_path, target_channels, target_samplerate, file_new_path)
+            os.system(command_to_run)
+
+            labels[filename] = class_dir
+
+    # Save labels dict
+    with open(os.path.join(path_new_dataset, 'labels.txt'), 'w') as outfile:
+        json.dump(labels, outfile)
+
+    print("Following files where under the energy threshold of {} and "
+          "were not loaded: {}".format(energy_threshold, ignored_audio))
+
+    return labels
+
+
 def correct_additional_utterances(path_additional, subfolder_name="corrected"):
     """A function to convert the additional utterances of the Zindi-Challenge to proper WAV files
 
