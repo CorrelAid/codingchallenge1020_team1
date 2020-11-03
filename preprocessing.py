a4cd8e6e97ba2451b5c0fd2ac6c6ba182d04e7bf
SteffenBe <Steffen-Berhorst@web.de>
2020-11-03 11:47:17 +0100

    Changed code to work with any sample rate

--- preprocessing.py
+++ preprocessing.py
@@ -11,9 +11,8 @@ except:
 
 
 def augment_sample(audio_sample_path, target_seconds=3, stretch_low=1.1, stretch_high=1.5, pitch_low=1.5, pitch_high=4,
-                   noise_low=0.001, noise_high=0.01):
+                   noise_low=0.001, noise_high=0.01, sr=48000):
 
-    sr = 48000
     target_len = target_seconds * sr
     audio_regions = split(audio_sample_path, sampling_rate=sr)
 
@@ -25,15 +24,15 @@ def augment_sample(audio_sample_path, target_seconds=3, stretch_low=1.1, stretch
         audio_regions_arrays.append(region_ar)
         audio_regions_info.append((audio_region.meta.start, audio_region.meta.end))
 
-    audio_regions_arrays, audio_regions_info = merge_regions(audio_regions_arrays, audio_regions_info)
+    audio_regions_arrays, audio_regions_info = merge_regions(audio_regions_arrays, audio_regions_info, sr=sr)
 
     total_len = np.sum([max(a.shape) for a in audio_regions_arrays])
     max_stretch = int(100 * target_len/total_len)/100
 
     stretch_high = min(max_stretch, stretch_high)
 
-    audio_regions_arrays = [time_stretch(ar, stretch_low, stretch_high) for ar in audio_regions_arrays]
-    audio_regions_arrays = [change_pitch(ar, pitch_low, pitch_high) for ar in audio_regions_arrays]
+    audio_regions_arrays = [time_stretch(ar, stretch_low, stretch_high, sr=sr) for ar in audio_regions_arrays]
+    audio_regions_arrays = [change_pitch(ar, pitch_low, pitch_high, sr=sr) for ar in audio_regions_arrays]
 
     total_len_after_stretch = np.sum([max(a.shape) for a in audio_regions_arrays])
     n_missing = int(target_len - total_len_after_stretch)
@@ -54,7 +53,7 @@ def augment_sample(audio_sample_path, target_seconds=3, stretch_low=1.1, stretch
         ar = np.concatenate([ar, noise_filler, audio_region_ar])
 
     final_ar = np.concatenate([noise_at_start, ar, noise_at_end])
-    final_ar = noise_injection(final_ar, noise_low, noise_high)
+    final_ar = noise_injection(final_ar, noise_low, noise_high, sr=sr)
 
     if len(final_ar) > target_len:
         final_ar = final_ar[:target_len]
@@ -102,10 +101,10 @@ def noise_period(noise_len, low=0.001, high=0.01):
     return np.random.normal(0, noise_level, noise_len)
 
 
-def time_stretch(audio_sample, low=1.1, high=1.5):
+def time_stretch(audio_sample, low=1.1, high=1.5, sr=48000):
 
     if isinstance(audio_sample, str) and audio_sample.split(".")[-1] == ".wav":
-        audio_sample = librosa.load(audio_sample, sr=48000)
+        audio_sample = librosa.load(audio_sample, sr=sr)
 
     stretch_factor = np.random.uniform(low, high)
 
@@ -115,20 +114,20 @@ def time_stretch(audio_sample, low=1.1, high=1.5):
     return librosa.effects.time_stretch(audio_sample, stretch_factor)
 
 
-def change_pitch(audio_sample, low=1.5, high=4):
+def change_pitch(audio_sample, low=1.5, high=4, sr=48000):
 
     if isinstance(audio_sample, str) and audio_sample.split(".")[-1] == ".wav":
-        audio_sample = librosa.load(audio_sample, sr=48000)
+        audio_sample = librosa.load(audio_sample, sr=sr)
 
     pitch_factor = np.random.uniform(low, high) * random.choice([1, -1])
 
-    return librosa.effects.pitch_shift(audio_sample, sr=48000, n_steps=pitch_factor)
+    return librosa.effects.pitch_shift(audio_sample, sr=sr, n_steps=pitch_factor)
 
 
-def noise_injection(audio_sample, low=0.001, high=0.01):
+def noise_injection(audio_sample, low=0.001, high=0.01, sr=48000):
 
     if isinstance(audio_sample, str) and audio_sample.split(".")[-1] == ".wav":
-        audio_sample = librosa.load(audio_sample, sr=48000)
+        audio_sample = librosa.load(audio_sample, sr=sr)
 
     noise_level = np.random.uniform(low, high)
     noise = np.random.normal(0, noise_level, len(audio_sample))
